7fab5de224ad02bdfedc40f8d9c94371edd50ea2
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2020-12-04 13:29:02 +0530

    Add userscript header

---
+++ betterslt.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Better SLT Portal
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Makes the stupid SLT Usage meter much much more user friendly
+// @author       m4heshd
+// @match        https://internetvas.slt.lk/dashboard
+// @grant        none
+// ==/UserScript==
