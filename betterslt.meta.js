514fef9a8e1a9a50cee46e349d3a26053dc57bcd
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2020-12-04 14:01:01 +0530

    Add meta file and update URLs

---
+++ betterslt.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Better SLT Portal
+// @namespace   https://github.com/m4heshd
+// @version     1.2
+// @updateURL   https://raw.githubusercontent.com/m4heshd/better-slt-portal/master/betterslt.meta.js
+// @downloadURL https://raw.githubusercontent.com/m4heshd/better-slt-portal/master/betterslt.user.js
+// @supportURL  https://github.com/m4heshd/better-slt-portal/issues
+// @description Makes the stupid SLT Broadband portal much much more user friendly
+// @author      m4heshd
+// @match       https://internetvas.slt.lk/dashboard
+// @grant       none
+// ==/UserScript==
--- betterslt.user.js
+++ betterslt.user.js
@@ -2,7 +2,7 @@
 // @name        Better SLT Portal
 // @namespace   https://github.com/m4heshd
 // @version     1.2
-// @updateURL   https://raw.githubusercontent.com/m4heshd/better-slt-portal/master/betterslt.user.js
+// @updateURL   https://raw.githubusercontent.com/m4heshd/better-slt-portal/master/betterslt.meta.js
 // @downloadURL https://raw.githubusercontent.com/m4heshd/better-slt-portal/master/betterslt.user.js
 // @supportURL  https://github.com/m4heshd/better-slt-portal/issues
 // @description Makes the stupid SLT Broadband portal much much more user friendly
