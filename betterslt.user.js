3a68ecfb93baa16538da4d7be2df3b296d27cc22
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2020-12-04 13:30:39 +0530

    Add functionality for package data usage

--- betterslt.user.js
+++ betterslt.user.js
@@ -7,3 +7,42 @@
 // @match        https://internetvas.slt.lk/dashboard
 // @grant        none
 // ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let stdSel = "#root > div > div > div:nth-child(3) > div > div > div > div:nth-child(3) > div.col-md-8 > div > div:nth-child(1) > div > div > div > div:nth-child(1) > div > div > div:nth-child(3) > svg > text";
+
+    let totSel = "#root > div > div > div:nth-child(3) > div > div > div > div:nth-child(3) > div.col-md-8 > div > div:nth-child(1) > div > div > div > div:nth-child(2) > div > div > div:nth-child(3) > svg > text";
+
+    let xhrnew = null;
+
+    function updUI(pkgData) {
+        let std = document.querySelector(stdSel);
+        std.innerHTML = pkgData.usageDetails["0"].remaining + "GB";
+
+        let tot = document.querySelector(totSel);
+        tot.innerHTML = pkgData.usageDetails["1"].remaining + "GB";
+    }
+
+    let req = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function () {
+        this.addEventListener('load', function () {
+            if (this.responseURL === "https://omniscapp.slt.lk/mobitelint/slt/sltvasservices/dashboard/mypackage") {
+                xhrnew = this;
+                let pkgData = JSON.parse(this.responseText);
+                setTimeout(function () {
+                    updUI(pkgData);
+                }, 200);
+            }
+            if (this.responseURL === "https://omniscapp.slt.lk/mobitelint/slt/sltvasservices/dashboard/summary") {
+                xhrnew = this;
+                let pkgData = JSON.parse(this.responseText);
+                setTimeout(function () {
+                    updUI(pkgData.my_package_info);
+                }, 200);
+            }
+        });
+        req.apply(this, arguments);
+    };
+})();
